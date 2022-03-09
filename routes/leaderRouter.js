1d4c5be45afdbea0c0fde3309d2f9b9ee7e4cbe2
SabbirRoman <95046730+SabbirRoman@users.noreply.github.com>
2022-03-09 16:09:57 +0600

    updating rest Api

---
+++ routes/leaderRouter.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const leaderRouter = express.Router();
+
+
+//learderRouter route 
+leaderRouter.get("/", (req, res) => {
+  res.send(`Will send all the leaders to You.`);
+});
+
+//leader router id method 
+leaderRouter.get("/:leaderId", (req, res) => {
+  res.send(`Will send leader of id: ${req.params.leaderId}`);
+});
+
+//leader router post method
+leaderRouter.post("/", (req, res) => {
+  res.send(`Will add the leader: ${req.body.name} with details: ${req.body.description}`);
+});
+
+//leader router post id method 
+leaderRouter.post("/:leaderId", (req, res) => {
+  res.status(403).send(`Post operation is not supported on /leaders/${req.params.leaderId}`);
+});
+
+leaderRouter.put("/:leaderId", (req, res) => {
+  res.write(`Updating the leader: ${req.params.leaderId}`);
+  res.end(`Will update the leader: ${req.body.name} with details: ${req.body.description}`);
+});
+
+leaderRouter.put("/", (req, res) => {
+  res.status(403).send(`Put operation is not supported on /leaders`);
+});
+
+leaderRouter.delete("/", (req, res) => {
+  res.send(`Will delete all the leaders!!!!`);
+});
+
+leaderRouter.delete("/:leaderId", (req, res) => {
+  res.send(`Will delete the leader of id: ${req.params.leaderId}`);
+});
+
+
+module.exports = leaderRouter;
---
+++ routes/promoRouter.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const promoRouter = express.Router();
+
+
+//promo router 
+promoRouter.get("/", (req, res) => {
+  res.send(`Will send all the promotions to You...`);
+});
+
+promoRouter.get("/:promoId", (req, res) => {
+  res.send(`Will send the promotion of id : ${req.params.promoId}`);
+});
+
+promoRouter.post("/", (req, res) => {
+  res.send(`Will add the promotion: ${req.body.name} with details : ${req.body.description}`);
+});
+
+promoRouter.post("/:promoId", (req, res) => {
+  res.status(403).send(`Post operation is not supported on /promotions/${req.params.promoId}`);
+});
+
+promoRouter.put("/:promoId", (req, res) => {
+  res.write(`Updating the promotion: ${req.params.promoId} \n`);
+  res.end(`Will update the promotion: ${req.body.name} with details: ${req.body.description}`);
+});
+
+promoRouter.put("/", (req, res) => {
+  res.status(403).send(`Put operation is not supported on /promotions`);
+});
+
+promoRouter.delete("/", (req, res) => {
+  res.send(`Will delete all the promotions!!!!`);
+});
+
+promoRouter.delete("/:promoId", (req, res) => {
+  res.send(`Will delete the promotion of Id: ${req.params.promoId}`);
+});
+
+
+module.exports = promoRouter;
